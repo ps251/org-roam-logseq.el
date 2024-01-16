ps251/org-roam-logseq.el

commit 0f7c85e43fcefade7405d1792c12633586fb4465

fix logical errors

org-roam-logseq.el
@@ -49,7 +49,7 @@
 (defcustom org-roam-logseq/ignore-journal-files t "When non-nil, journal files will be ignored")
 
 
-(defcustom org-roam-logseq/logseq-id-title-mod-path (f-expand (f-join org-roam-directory "pages")) "paths where id and title additions are allowed")
+(defcustom org-roam-logseq/logseq-id-title-mod-path (f-expand (f-join org-roam-directory "pages/")) "paths where id and title additions are allowed")
 
 (defcustom org-roam-logseq/ignore-file-links t "When non-nil, file-links will not be converted, only fuzzy links")
 
@@ -58,10 +58,13 @@
 (defun org-roam-logseq-ensure-file-id (file)
   "Visit an existing file, ensure it has an id, return whether the a new buffer was created"
   (setq file (f-expand file))
-  (if (and (and org-roam-logseq/ignore-journal-files (org-roam-logseq-logseq-journal-p file) ) (not (string-match-p (concat "^" org-roam-logseq/logseq-id-title-mod-path) file)))
+  (if (or (and org-roam-logseq/ignore-journal-files (org-roam-logseq-logseq-journal-p file) ) (not (string-match-p (concat "^" org-roam-logseq/logseq-id-title-mod-path) file)))
       ;; do nothing for journal files if org-roam-logseq/ignore-journal-files is non-nil
       ;; TODO double check this is actually desired behaviour
-      `(nil . nil)
+      (if-let ( (buf (get-file-buffer file)) )
+          (cons nil buf)
+        (cons t (find-file-noselect file))
+        )
     (let* ((buf (get-file-buffer file))
            (was-modified (buffer-modified-p buf))
            (new-buf nil)
@@ -147,7 +150,7 @@
                            (buffer-substring-no-properties contents-begin contents-end)
                            (org-element-property :raw-link link)
                          )))
-      (when (and org-roam-logseq/ignore-file-links (equal "file" (org-element-property :type link)))
+      (when (and (not org-roam-logseq/ignore-file-links) (equal "file" (org-element-property :type link)))
         ;; TODO create a workaround for Logseq's bug with aliases
         (setq filename (f-expand (replace-regexp-in-string "\\..//" "/" (org-element-property :path link))))
         (if (org-element-property :contents-begin link)

commit 39fa50459bbd60eae9e3c49055341436642b84e0

Fix logical error regression
See https://github.com/FrederickGeek8/org-roam-logseq.el/pull/1
for more information.

There is a small bug with the boolean logic that
was introduced in a previous commit as a "fix".
I think it can be a bit confusing because we
trigger a null-action if the if statement is true,
indicating that we should skip processing the file.

Because of this bug, enabling journal modification
would not take effect, since the journal is not
"in the search path" that is checked in the
second part of the "or" statement.

We want:
- if (skip-journal and not-path) skip

Instead we have
- if (skip-journal or not-path) skip

If we wanted to include a journal (skip-journal=False)
then it would be excluded if the journals are not
in the "search path".

This bug was introduced in commit
0f7c85e43fcefade7405d1792c12633586fb4465

org-roam-logseq.el
@@ -80,7 +80,7 @@
   ;; do nothing at all when file is excluded by exclude pattern
   (if (string-match-p org-roam-logseq/logseq-exclude-pattern (file-truename file))
       (cons nil nil)
-    (if (or (and org-roam-logseq/ignore-journal-files (org-roam-logseq/logseq-journal-p file) ) (not (string-match-p (concat "^" org-roam-logseq/logseq-id-title-mod-path) file)))
+    (if (and (and org-roam-logseq/ignore-journal-files (org-roam-logseq/logseq-journal-p file) ) (not (string-match-p (concat "^" org-roam-logseq/logseq-id-title-mod-path) file)))
         ;; do not add id and title for journal files if org-roam-logseq/ignore-journal-files is non-nil. Also if file doesn't match org-roam-logseq/logseq-id-title-mod-path
         (if-let ( (buf (get-file-buffer file)) )
             (cons nil buf)